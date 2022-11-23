200c5b2831b9d5cbccc9bc34d0671a5f396d695c
Brandon Nguyen <BrandonNguyen55@gmail.com>
2022-11-22 23:52:31 -0800

    Testing out the eyed3 audio components

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python
-
+# Library Imports
 import string
 import os
 import eyed3
 
-cwd = os.getcwd()
-
+# Global variables
+CWD = os.getcwd()
+TEST_DIR = os.path.join(CWD, "..", "test", "music")
 #===========================================================================================
 #Main Function
 #===========================================================================================
 def main():
-    # audio = eyed3.load(cwd)
+    audio = eyed3.load(os.path.join(TEST_DIR, "Rock", "Eric Johnson - Cliffs Of Dover.mp3"))
 
-
-    print(cwd)
+    print(f"Title: {audio.tag.title}")
+    print(f"Artist: {audio.tag.artist}")
+    print(f"Album: {audio.tag.album}")
+    print(f"Album Artist: {audio.tag.album_artist}")
+    print(f"Composer: {audio.tag.composer}")
+    print(f"Publisher: {audio.tag.publisher}")
+    print(f"Genre: {audio.tag.genre}")
 
     return 0
 
