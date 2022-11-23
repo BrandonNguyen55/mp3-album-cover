b7e021c271d473da28f6757b4a15214c4c032939
Brandon Nguyen <BrandonNguyen55@gmail.com>
2022-11-22 23:04:29 -0800

    Set up the main python script

---
+++ main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import string
+import os
+import eyed3
+
+cwd = os.getcwd()
+
+#===========================================================================================
+#Main Function
+#===========================================================================================
+def main():
+    # audio = eyed3.load(cwd)
+
+
+    print(cwd)
+
+    return 0
+
+
+main()
