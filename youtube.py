f3e0c8fb6398ea1241c780214062695cdfe8e4ff
Brandon Nguyen <BrandonNguyen55@berkeley.edu>
2023-02-17 22:44:48 -0800

    feat: Add yt_dlp to allow downloading youtube videos into mp3 files

---
+++ youtube.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+import yt_dlp
+
+URLS = ['https://www.youtube.com/watch?v=6nNEGElRfwY']
+
+class MyLogger:
+    def debug(self, msg):
+        # For compatibility with youtube-dl, both debug and info are passed into debug
+        # You can distinguish them by the prefix '[debug] '
+        if msg.startswith('[debug] '):
+            pass
+        else:
+            self.info(msg)
+
+    def info(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        print(msg)
+
+
+# ℹ️ See "progress_hooks" in help(yt_dlp.YoutubeDL)
+def my_hook(d):
+    if d['status'] == 'finished':
+        print('Done downloading, now post-processing ...')
+
+
+ydl_opts = {
+    'logger': MyLogger(),
+    'progress_hooks': [my_hook],
+    'format': 'm4a/bestaudio/best',
+    # See help(yt_dlp.postprocessor) for a list of available Postprocessors and their arguments
+    'postprocessors': [{'key': 'FFmpegExtractAudio',
+                        'preferredcodec': 'm4a',
+                       }]
+}
+
+
+
+def download_youtube(urls):
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download(urls)
